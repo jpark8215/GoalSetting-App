44115e92d05a5091ff50fc45c2da57312ef89f64
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2023-07-18 21:31:11 -0400

    Add files via upload

---
+++ src/main/java/com/example/jieungoalsettingapp/ui/home/HomeFragment.kt
@@ -0,0 +1,47 @@
+package com.example.jieungoalsettingapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.jieungoalsettingapp.databinding.FragmentDashboardBinding
+import com.example.jieungoalsettingapp.databinding.FragmentHomeBinding
+import com.example.jieungoalsettingapp.ui.dashboard.DashboardViewModel
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
