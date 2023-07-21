569552880b38f0c9912f8b560ff931680f8c1fd2
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2023-07-20 22:49:28 -0400

    Add files via upload

--- src/main/java/com/example/jieungoalsettingapp/MainActivity.kt
+++ src/main/java/com/example/jieungoalsettingapp/MainActivity.kt
@@ -1,32 +1,30 @@
 package com.example.jieungoalsettingapp
 
 import android.os.Bundle
-import android.widget.Button
-import com.google.android.material.textfield.TextInputEditText
-import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.jieungoalsettingapp.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var editTextUserInput: TextInputEditText
-    private lateinit var buttonSubmit: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
@@ -34,17 +32,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        editTextUserInput = binding.textInput.editText as TextInputEditText
-        buttonSubmit = binding.buttonSubmit
-
-        buttonSubmit.setOnClickListener {
-            val userInput = editTextUserInput.text.toString()
-            showToast("User input: $userInput")
-        }
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
+
+
--- src/main/java/com/example/jieungoalsettingapp/ui/home/HomeFragment.kt
+++ src/main/java/com/example/jieungoalsettingapp/ui/home/HomeFragment.kt
@@ -5,43 +5,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.jieungoalsettingapp.databinding.FragmentDashboardBinding
 import com.example.jieungoalsettingapp.databinding.FragmentHomeBinding
-import com.example.jieungoalsettingapp.ui.dashboard.DashboardViewModel
+import java.net.URLEncoder
 
 class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var editTextUserInput: TextInputEditText
+    private lateinit var buttonSubmit: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        editTextUserInput = _binding?.textInput?.editText as? TextInputEditText ?: return root
+        buttonSubmit = _binding?.buttonSubmit ?: return root
+
+        buttonSubmit.setOnClickListener {
+            val userInput = editTextUserInput.text.toString()
+            performGoogleSearch(userInput)
+        }
+
         val textView: TextView = binding.textHome
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
+    }
+
+
+    private fun performGoogleSearch(query: String) {
+        val encodedQuery = URLEncoder.encode(query, "utf-8")
+        val searchUrl = "https://www.google.com/search?q=$encodedQuery"
+        // You can open this URL in a WebView or a browser to display the Google search results.
+        // For simplicity, we'll just show a Toast with the URL.
+        showToast("Performing Google search: $searchUrl")
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
