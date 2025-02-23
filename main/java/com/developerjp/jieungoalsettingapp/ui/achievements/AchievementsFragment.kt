c7c68633ce4abd2f2776d4d8e049d68e6aef6946
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2025-02-23 16:51:40 -0500

    Add files via upload

--- main/java/com/developerjp/jieungoalsettingapp/ui/achievements/AchievementsFragment.kt
+++ main/java/com/developerjp/jieungoalsettingapp/ui/achievements/AchievementsFragment.kt
@@ -9,12 +9,15 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.developerjp.jieungoalsettingapp.data.DBHelper
 import com.developerjp.jieungoalsettingapp.databinding.FragmentAchievementsBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class AchievementsFragment : Fragment() {
     private var _binding: FragmentAchievementsBinding? = null
     private val binding get() = _binding!!
     private lateinit var achievementsViewModel: AchievementsViewModel
     private lateinit var adapter: AchievementsViewModel.CompletedGoalsAdapter
+    private lateinit var adView: AdView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,20 +31,31 @@ class AchievementsFragment : Fragment() {
         )[AchievementsViewModel::class.java]
 
         _binding = FragmentAchievementsBinding.inflate(inflater, container, false)
+        
+        // Initialize and load the ad
+        adView = binding.adView
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
 
         setupViews()
         observeViewModel()
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        achievementsViewModel.refreshData()
+    override fun onPause() {
+        adView.pause()
+        super.onPause()
     }
 
     override fun onResume() {
         super.onResume()
+        adView.resume()
         achievementsViewModel.refreshData()
+    }
+
+    override fun onDestroy() {
+        adView.destroy()
+        super.onDestroy()
     }
 
     private fun setupViews() {
